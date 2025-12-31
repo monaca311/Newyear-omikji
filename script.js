99b4706042c02828ef902ae76e852d7e36435436
monaca311 <hinata060311@icloud.com>
2025-12-31 11:52:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -32,31 +32,13 @@ let isShaking = false; // 連続してカウントしないためのフラグ
 function startSensor() {
     window.addEventListener('devicemotion', (event) => {
         const acc = event.accelerationIncludingGravity;
+        if (!acc || !acc.x) return; 
 
-        if(!acc.x) return;
-        
         const totalAccel = Math.abs(acc.x) + Math.abs(acc.y) + Math.abs(acc.z);
 
-        // しきい値を超えた、かつ「今振っている最中」でなければカウント
-        if (totalAccel >15 && !isShaking) {
-            shakeCount++;
-            isShaking = true; // 一度カウントしたら、一旦「振り中」にする
-
-            // 0.5秒後に「振り中」を解除（連続カウント防止）
-            setTimeout(() => {
-                isShaking = false;
-            }, 500);
-
-            if(shakecCount == 1) {
-                display.textContent = "ガシャ...";
-            }
-
-            // 2回（以上）振られたらおみくじを引く！
-            if (shakeCount >= 2) {
-                drawOmikuji();
-                shakeCount = 0; // 回数をリセット
-            }
+        // 15まで下げて、かつif文を一つだけにします
+        if (totalAccel > 15) {
+            drawOmikuji();
         }
     });
 }
-
